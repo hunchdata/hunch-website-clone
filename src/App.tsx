882466cfb8b44424e3f40b26e633e54f41ebf6a6
Alex L <aleib.start@gmail.com>
2025-12-06 15:43:06 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <GitHubPagesRedirectHandler />
         <Routes>
           <Route path="/" element={<Index />} />
